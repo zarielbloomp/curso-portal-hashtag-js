26d0b3240c47714ac0d9a3ff8764ae926921f36e
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-06 19:07:15 -0300

    Update ex001.js

--- mod3/ex001.js
+++ mod3/ex001.js
@@ -6,7 +6,7 @@ let numeroInical2 = 0;
 
 
 console.log(
-  `O valor da primeira variavel e ${numeroInical} substituindo o termo iniciado pelo indicado em $ pelo valor obrigado na sua variavel`
+  `O valor da primeira variavel e ${numeroInical}`
 );
 
 numeroInical2 = 2;
@@ -23,4 +23,17 @@ numeroInical2 = numeroInical2/3;
 console.log(`O valor da segunda variavel, apos ser elevada ao quadrado, e dividida por 3, e igual a ${numeroInical2}`);
 
 
+console.log('Resolução do ex2 do mod3')
 
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(`Operação AND entre as duas variaveis: ${boolean1 && boolean2}`);
+
+console.log(`Operação AND entre as duas variaveis: ${boolean1 || boolean2}`);
+
+const boolean3 = false;
+
+console.log(`Operação AND entre as três variaveis: ${boolean1 && boolean2 && boolean3}`);
+
+console.log(`Operação AND entre as três variaveis: ${boolean1 || boolean2 || boolean3}`);
