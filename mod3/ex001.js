4028689f9a330b837cf328c784aba587519aaa55
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-06 18:25:56 -0300

    uptade ex001

---
+++ mod3/ex001.js
@@ -0,0 +1,26 @@
+console.log('Resolução do ex1 do mod3')
+
+
+const numeroInical = 2148;
+let numeroInical2 = 0;
+
+
+console.log(
+  `O valor da primeira variavel e ${numeroInical} substituindo o termo iniciado pelo indicado em $ pelo valor obrigado na sua variavel`
+);
+
+numeroInical2 = 2;
+let valorFinal =(numeroInical + numeroInical2)
+
+console.log(`Minha segunda variavel tem o valor de ${numeroInical2} e a soma das duas variaves e igual a ${valorFinal}`);
+
+numeroInical2 = numeroInical2**2;
+
+console.log(`O valor da segunda variavel, elevado ao quadrado e igual a ${numeroInical2}`);
+
+numeroInical2 = numeroInical2/3;
+
+console.log(`O valor da segunda variavel, apos ser elevada ao quadrado, e dividida por 3, e igual a ${numeroInical2}`);
+
+
+
--- mod3/index.js
+++
@@ -1,4 +0,0 @@
-let mensagem = "Hello Word!!";
-const mensagem2 = "Hello Word 2!";
-
-console.log(mensagem + mensagem2);
