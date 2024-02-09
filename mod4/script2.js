3d743ce723c9055771f3b7fa3bac0e4667ca93a3
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-09 16:41:34 -0300

    testando argumentos e parametros

---
+++ mod4/script2.js
@@ -0,0 +1,8 @@
+function calcularQuadrado(numeroDaVez/*parametro*/){
+
+    console.log(`O numero da vez e o: ${numeroDaVez}`)
+    console.log(`O número da vez e (${numeroDaVez}), elevado ao quadrado e: ${numeroDaVez **2}`)
+
+}
+
+calcularQuadrado(1/*argumento*/);
