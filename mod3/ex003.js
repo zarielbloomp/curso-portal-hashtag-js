cbb8e22d433c43e2bbddc14aab1f97e233348abc
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-09 16:31:44 -0300

    cabei o mod 3

--- mod3/ex003.js
+++ mod3/ex003.js
@@ -1,9 +1,16 @@
 console.log("Resultado do ex003, do mod3");
 
-let pokemon1 = {
+const pokemon1 = {
     nome: 'Rayquaza',
     speed: 115,
     defesa: 100,
-    ataque:   ,
-    habilidade:  
+    ataque:   150,
+    habilidade: 'Air Lock' 
 }
+
+
+console.log(`Meu objeto pokemon possui o nome: ${pokemon1.nome}`);
+console.log(`Meu objeto pokemon possui a speed de: ${pokemon1.speed}`);
+console.log(`Meu objeto pokemon possui a defesa de: ${pokemon1.defesa}`);
+console.log(`Meu objeto pokemon possui o ataque de: ${pokemon1.ataque}`);
+console.log(`Meu objeto pokemon possui a habilidade: ${pokemon1.habilidade}`);
---
+++ mod3/ex004.js
@@ -0,0 +1,37 @@
+console.log("O resultado do ex004 mod3");
+
+let num1 = 133;
+let num2 = 45;
+
+const sum = num1 + num2;
+console.log(sum);
+const sub = num1 - num2;
+console.log(sub);
+const sub2 = num2 - num1;
+console.log(sub2);
+const div = num1 / num2;
+console.log(div);
+const mult = num1 * num2;
+console.log(mult);
+
+num1 = 33;
+num2 = 222;
+console.log(num1 > num2);
+console.log(num2 > num1);
+
+num1 = 88;
+num2 = 55;
+
+console.log(num1 < num2);
+console.log(num2 < num1);
+
+num1 = "123";
+num2 = 123;
+
+console.log(num1 === num2);
+console.log(num1 == num2); //não e bom
+
+num1 = 10;
+num2 = 10;
+console.log(num1 === num2);
+console.log(num1 == num2);
