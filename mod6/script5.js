597cb99e72a8e5595a0cc38353de52f0d1e433e7
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-19 17:04:26 -0300

    Create script5.js

---
+++ mod6/script5.js
@@ -0,0 +1,23 @@
+function calculateFibonnaci(termNumber) {
+  let lastTerm = 1;
+  let secontToLast = 0;
+
+  if (termNumber === 0) {
+    return 0;
+  }
+
+  if (termNumber === 1) {
+    return 1;
+  }
+
+  let currentIteration = 2;
+  while (currentIteration <= termNumber) {
+    let auxVar = lastTerm + secontToLast;
+    secontToLast = lastTerm;
+    lastTerm = auxVar;
+    currentIteration++;
+  }
+  return lastTerm;
+}
+
+console.log(`O valor numero 6 da sequencia e ${calculateFibonnaci(6)}`);
