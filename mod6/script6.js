84ba625951585a419da5066d80f93d77c569e6cd
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-21 22:20:06 -0300

    Create script6.js

---
+++ mod6/script6.js
@@ -0,0 +1,15 @@
+const regularArray = [2, 5, 2, 7, 1, 3, 4, 7];
+ 
+/*let count =0;
+
+while(count < regularArray.length){
+    regularArray[count] += count;
+    count++;
+}*/
+
+for(let count = 0; count < regularArray.length; count++){
+    regularArray[count] += count;
+}
+
+
+console.log('O array atual é:', regularArray);
